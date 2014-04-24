b2980db8a9a80c98db25bb456d0fb2d5e43c1564
wuniyunyao <nianyuediemeng@yahoo.com>
2014-04-24 17:35:48 +0800

    2014/4/24
    StaticBlockObject还没有添加完

--- Utils/Box2DTMXTool.h
+++ Utils/Box2DTMXTool.h
@@ -1,7 +1,9 @@
 #ifndef __BOX2DTMXTOOL_H_
 #define __BOX2DTMXTOOL_H_
+#define PTM_RATIO 32
 #include "cocos2d.h"
 #include "Box2D.h"
+using namespace cocos2d;
 /*
 文件名：Box2DTMLTool.h
 描述：该工具用于将tiled map中的地形读出供Box2D使用
@@ -11,7 +13,12 @@
 class Box2DTMXTool{
 public:
 	bool readPhysicalMap(b2World *b2world);
+	bool setTiledMap(CCTMXTiledMap* tiledMap);
+	CCTMXTiledMap* getTiledMap();
+	bool readTiledMapForBlocks(b2World* world);
 private:
-	CC_SYNTHESIZE(CCTMXTiledMap*,_mTiledMap,TiledMap);
+	CCTMXTiledMap* mTiledMap;
+	b2Body* mBody;
+	
 };
 #endif //__BOX2DTMXTOOL_H_
---
+++ Utils/StaticBlockObject.h
@@ -0,0 +1,18 @@
+#ifndef __STATICBLOCKOBJECT_H_
+#define __STATICBLOCKOBJECT_H_
+#define PTM_RATIO 32
+#include "cocos2d.h"
+#include "Box2D.h"
+using namespace cocos2d;
+/*
+文件名：StaticBlocObject.h
+描述：该类用于存储静态阻碍
+创建者：无逆云遥
+创建日期：2013/4/24
+*/
+class StaticBlockObject : public b2BodyDef{
+public:
+private:
+	
+};
+#endif //__STATICBLOCKOBJECT_H_
